d74a08961e225abd3aa4b986f7d4d77b876be35a
Chloe Colman <chloe.colman7@gmail.com>
2026-09-20 01:04:56 -0400

    move keyball39 miryoku autocorrect dictionary into shared users/miryoku userspace

---
+++ users/miryoku/rules.mk
@@ -0,0 +1,4 @@
+# Shared by every keymap named "miryoku" (keyball39, killerwhale/duo).
+# Regenerate the header after editing the dictionary:
+#   qmk generate-autocorrect-data users/miryoku/autocorrect_dictionary.txt -o users/miryoku/autocorrect_data.h
+AUTOCORRECT_ENABLE = yes
